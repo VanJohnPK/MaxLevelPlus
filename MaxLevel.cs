7ef07530397d5f57d9f365a4952fff18bd39dec8
darkVinci <1585781755@qq.com>
2025-04-10 18:59:44 +0800

    update for 9.2.11-vm

--- MaxLevel.cs
+++ MaxLevel.cs
@@ -9,10 +9,10 @@ namespace VanMaxLevel;
 public class VanMaxLevel : Mod
 {
     public override string Author => "darkVinci";
-    public override string Name => "MaxLevel 97";
-    public override string Description => "Increase the level limit to 97";
-    public override string Version => "1.0.0.0";
-    public override string TargetVersion => "0.9.1.19-vm";
+    public override string Name => "MaxLevel 100";
+    public override string Description => "Increase the level limit to 100";
+    public override string Version => "1.0.0.1";
+    public override string TargetVersion => "0.9.2.11-vm";
 
     public override void PatchMod()
     {
@@ -23,12 +23,12 @@ public class VanMaxLevel : Mod
     {
         Msl.LoadAssemblyAsString("gml_Object_o_player_Step_0")
             .MatchFrom("pushi.e 30\ncmp.i.v EQ")
-            .ReplaceBy("pushi.e 97\ncmp.i.v EQ")
+            .ReplaceBy("pushi.e 100\ncmp.i.v EQ")
             .Save();
 
         Msl.LoadGML("gml_Object_o_character_panel_mask_Draw_0")
-            .MatchFrom("var _maxLevel = (scr_atr(\"LVL\") == 30)")
-            .ReplaceBy("var _maxLevel = (scr_atr(\"LVL\") == 97)")
+            .MatchFrom("var _maxLevel = scr_atr(\"LVL\") == 30")
+            .ReplaceBy("var _maxLevel = scr_atr(\"LVL\") == 100")
             .Save();
     }
 }
